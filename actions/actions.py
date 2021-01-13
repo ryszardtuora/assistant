173b0fd9a13930a62c73ec980ce64ff6938bbb70
Ryszard Tuora <ryszardtuora@gmail.com>
2021-01-13 18:56:58 +0100

    dodawanie kontaktów

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,46 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+import spacy
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+from contact_utils import extract_name, add_contact
+
+nlp = spacy.load("pl_spacy_model_morfeusz")
+test_doc = nlp("to jest dokument testowy")
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Hello World!")
+        return []
+
+
+class ActionAddContact(Action):
+
+    def name(self) -> Text:
+        return "action_add_contact" # definiujemy nazwę akcji dla RASY
+
+    def run(self, dispatcher: CollectingDispatcher,
+                  tracker: Tracker,
+                  domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        message = tracker.latest_message["text"] # pobranie wiadomości od użytkownika
+        extracted_name = extract_name(message, nlp) # ekstrakcja znormalizowanego nazwiska i imienia
+        if extracted_name: # ekstrakcja się udała -> zapisujemy dane w pliku
+            add_contact(extracted_name)
+            confirmation_message = "Dodano kontakt: {}. Czy wszystko się zgadza?".format(extracted_name)
+            dispatcher.utter_message(text=confirmation_message)
+        else: # ekstrakcja się nie udała, prosimy o ponowienie
+            error_message "Nie zrozumiałem imienia lub nazwiska tej osoby, proszę powtórz polecenie."
+            dispatcher.utter_message(text=error_message)
+        return [SlotSet("person", extracted_name)]
+
