845a0ed76c3e6a9f285cbdd5937bb20a0754c35e
Ryszard Tuora <ryszardtuora@gmail.com>
2021-01-14 23:52:24 +0100

    formularz dla kontaktów

--- actions/actions.py
+++ actions/actions.py
@@ -13,6 +13,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+from rasa_sdk.forms import FormAction
 
 from contact_utils import extract_name, add_contact
 from api_utils import get_currency_rates
@@ -32,24 +33,30 @@ class ActionHelloWorld(Action):
         return []
 
 
-class ActionAddContact(Action):
+class ContactForm(FormAction):
+    def name(self) -> Text:
+        return "contact_form"
+
+    def required_slots(tracker):
+        return ["person", "email"]        
+
+
+class ActionSubmitContactForm(Action):
 
     def name(self) -> Text:
-        return "action_add_contact" # definiujemy nazwę akcji dla RASY
+        return "action_submit_contact_form"
 
     def run(self, dispatcher: CollectingDispatcher,
                   tracker: Tracker,
                   domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        message = tracker.latest_message["text"] # pobranie wiadomości od użytkownika
-        extracted_name = extract_name(message, nlp) # ekstrakcja znormalizowanego nazwiska i imienia
-        if extracted_name: # ekstrakcja się udała -> zapisujemy dane w pliku
-            add_contact(extracted_name)
-            confirmation_message = "Dodano kontakt: {}.".format(extracted_name)
-            dispatcher.utter_message(text=confirmation_message)
-        else: # ekstrakcja się nie udała, prosimy o ponowienie
-            error_message  = "Nie zrozumiałem imienia lub nazwiska tej osoby, proszę powtórz polecenie."
-            dispatcher.utter_message(text=error_message)
-        return [SlotSet("person", extracted_name)]
+        slot_name = tracker.get_slot("person")
+        email = tracker.get_slot("email")
+        name = extract_name(slot_name, nlp)
+        data = {"name": name, "email": email}
+        add_contact(data)
+        confirmation_message = "Dodano kontakt {} o emailu {}".format(name, email)
+        dispatcher.utter_message(confirmation_message)
+        return [SlotSet("person", name)]
 
 
 class ActionGetForex(Action):
@@ -63,6 +70,4 @@ class ActionGetForex(Action):
         rates = get_currency_rates()
         dispatcher.utter_message(text=rates)
         return []
-
-
 
